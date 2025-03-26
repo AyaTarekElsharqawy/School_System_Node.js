a802ca21569e4fdc7317b1155106ee085bf08dd0
Arwa Mustafa <143215370+arwaemam2@users.noreply.github.com>
2025-03-26 12:43:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors from "cors";
 import myConnection from "./Database/dbconnection.js";
 import customerRoute from './Modules/User/user.route.js';
 import ProductRoute from './Modules/Product/product.route.js';
@@ -9,21 +11,25 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
-
-app.use( customerRoute);
+app.use(customerRoute);
 app.use('/product', ProductRoute);
 app.use("/students", studentRoutes);
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 myConnection;
-
 const PORT = process.env.PORT || 3030;
 app.listen(3030, function(){
-    console.log("Server is running on port 3030");
+  console.log("Server is running on port 3030");
 });
